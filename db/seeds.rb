6797bd5e46bf38bf98bad5a7d04f6476fdb8dd22
Rails Udemy Course <14225425@life.hkbu.edu.hk>
2019-05-12 18:06:04 +0800

    Add user table with validates, install bcrypt and hirb gem, create some data in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+User.create(username: 'ravi', password: 'ravi')
+User.create(username: 'ravi1', password: 'ravi1')
+User.create(username: 'ravi2', password: 'ravi2')
+User.create(username: 'ravi3', password: 'ravi3')
+User.create(username: 'ravi4', password: 'ravi4')
